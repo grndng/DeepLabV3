60f2da66201bf3de9f5899abed696a6531d90f39
grndng <gueren.dinga@hcu-hamburg.de>
2023-09-25 09:50:53 +0200

    initial commit

---
+++ model.py
@@ -0,0 +1,25 @@
+""" DeepLabv3 Model download and change the head for your prediction"""
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+from torchvision import models
+import torch.nn as nn
+
+def createDeepLabv3(outputchannels=1):
+    """DeepLabv3 class with custom head
+
+    Args:
+        outputchannels (int, optional): The number of output channels
+        in your dataset masks. Defaults to 1.
+
+    Returns:
+        model: Returns the DeepLabv3 model with the ResNet101 backbone.
+    """
+
+    model = models.segmentation.deeplabv3_resnet50(pretrained=False,
+                                                    progress=True)
+    
+    model.backbone.conv1 = nn.Conv2d(5, 64, 7, 2, 3, bias = False)
+    
+    model.classifier = DeepLabHead(2048, outputchannels)
+    # Set the model in training mode
+    model.train()
+    return model
---
+++ predictor.py
@@ -0,0 +1,56 @@
+import torch
+import matplotlib.pyplot as plt
+import tifffile
+import pandas as pd
+
+model_MSE = torch.load("./Training_5ch/weights.pt")
+model_BCE = torch.load("./Training_5ch_BCE/weights.pt")
+model_MSE.eval()
+model_BCE.eval()
+
+df_MSE = pd.read_csv("./Training_5ch/log.csv")
+df_BCE = pd.read_csv("./Training_5ch_BCE/log.csv")
+
+# df_MSE.plot(x="epoch", figsize=(15,8))
+# plt.show()
+
+# df_BCE.plot(x='epoch',figsize=(15,8));
+# plt.show()
+
+# print(df_MSE[['Train_auroc','Test_auroc']].max())
+# print(df_BCE[['Train_auroc','Test_auroc']].max())
+
+#image_name = "dop10rgbih_32363_5651_1_nw_tile0_7168.tif"
+#img = tifffile.imread(f"C:/Users/dinga/Documents/Doktorat/2023_CD_TrainData_cleansed/02_test_images/{image_name}").transpose(2,0,1)
+#mask = tifffile.imread(f"C:/Users/dinga/Documents/Doktorat/2023_CD_TrainData_cleansed/02_test_masks/{image_name}")
+
+image_path = "C:/Users/dinga/Documents/Doktorat/DeepLabV3/TrainingData/Images/dop10rgbih_32363_5651_1_nw_tile0_7168.tif"
+mask_path = "C:/Users/dinga/Documents/Doktorat/DeepLabV3/TrainingData/Masks/dop10rgbih_32363_5651_1_nw_tile0_7168.tif"
+img = tifffile.imread(image_path).transpose(2,0,1)
+mask = tifffile.imread(mask_path)
+
+with torch.no_grad():
+    a = model_BCE(torch.from_numpy(img).unsqueeze(0).type(torch.cuda.FloatTensor)/255)
+
+plt.hist(a['out'].data.cpu().numpy().flatten())
+plt.show()
+
+#img = tifffile.imread(f"C:/Users/dinga/Documents/Doktorat/2023_CD_TrainData_cleansed/02_test_images/{image_name}")
+img = tifffile.imread(image_path)
+img = img[:,:,:3]
+
+plt.figure(figsize=(10,10))
+plt.subplot(131)
+plt.imshow(img)
+plt.title('Image')
+plt.axis('off')
+plt.subplot(132)
+plt.imshow(mask)
+plt.title('Ground Truth')
+plt.axis('off')
+plt.subplot(133)
+plt.imshow(a['out'].cpu().detach().numpy()[0][0]>0)
+plt.title('Segmentation Output')
+plt.axis('off')
+plt.show()
+#plt.savefig('./CFExp/SegmentationOutput.png',bbox_inches='tight')
