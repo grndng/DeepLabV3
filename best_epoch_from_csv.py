0fe344dee896d3f2d47cd815651bb054b4cc2812
grndng <gueren.dinga@hcu-hamburg.de>
2023-12-18 20:59:34 +0100

    simple utility, nevermind this

---
+++ best_epoch_from_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import torch
+from model import createDeepLabv3
+
+#csv_path = r"D:\Potsdam_Final\Training_Results\Potsdam_512_H\log.csv"
+csv_path = "./Training_5ch/log.csv"
+df = pd.read_csv(csv_path)
+
+print(df.head())
+print(df.loc[df['Test_f1_score'].idxmax()])
