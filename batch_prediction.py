e56f7d02437aebf2361bdae1690ae2e7d694ee2c
grndng <gueren.dinga@hcu-hamburg.de>
2023-12-18 21:00:40 +0100

    for deeplabv3 it's not necessary to divide results by 255

--- batch_prediction.py
+++ batch_prediction.py
@@ -1,61 +1,35 @@
 # TODO: get code working so I can predict multiple image files and get IoU statistics for each epoch
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import tifffile
 import torch
 import os
 
-def single_epoch_statistics(model_path: str, image_path: str, mask_path: str, pred_path: str, channels: int) -> int:
+def single_epoch_predictions(model_path: str, image_path: str, mask_path: str, pred_path: str, channels: int) -> int:
     model = torch.load(model_path)
     model.eval()
+
+    if not os.path.exists(pred_path):
+        os.mkdir(pred_path)
     
     for image_file, mask_file in zip(os.listdir(image_path),os.listdir(mask_path)):
         img = tifffile.imread(f"{image_path}/{image_file}")[:,:,:channels].transpose(2,0,1)
-        #mask = tifffile.imread(f"{mask_path}/{mask_file}")
+        mask = tifffile.imread(f"{mask_path}/{mask_file}")
 
         # make prediction
         with torch.no_grad():
-            pred = model(torch.from_numpy(img).unsqueeze(0).type(torch.cuda.FloatTensor)/255)
-            plt.figure(figsize=(10,10))
-            plt.imshow(pred['out'].cpu().detach().numpy()[0][0] > 0)
-            
-#            tifffile.imwrite(f"{pred_path}/{mask_file}", output_file)
+            pred = model(torch.from_numpy(img).unsqueeze(0).type(torch.cuda.FloatTensor))
+            threshold = 0 #np.median(pred['out'].data.cpu().numpy().flatten())
+            pred = np.invert(pred['out'].cpu().detach().numpy()[0][0] > threshold)
+            tifffile.imwrite(f"{pred_path}/{image_file}", pred)
 
 
-#     flattened_output = pred['out'].data.cpu().numpy().flatten()
-#     values, counts = np.unique(flattened_output, return_counts=True)
-
-#     plt.hist(flattened_output)
-#     plt.show()
-
-#     img = tifffile.imread(image_path)
-#     img = img[:,:,:3]
-
-#     threshold = 0
-
-# def plot_figure(image, mask, prediction, threshold):
-#     plt.figure(figsize=(10,10))
-#     plt.subplot(131)
-#     plt.imshow(image)
-#     plt.title('Image')
-#     plt.axis('off')
-#     plt.subplot(132)
-#     plt.imshow(mask)
-#     plt.title('Ground Truth')
-#     plt.axis('off')
-#     plt.subplot(133)
-#     plt.imshow(prediction['out'].cpu().detach().numpy()[0][0] > threshold) # wie wähl ich diesen Wert am besten
-#     plt.title('Segmentation Output')
-#     plt.axis('off')
-#     plt.show()
-
-# plot_figure(img, mask, pred, threshold)
-
-single_epoch_statistics(
-    model_path = r"D:\Potsdam_Final\Training_Results\Potsdam_512_H\weights.pt",
-    image_path = r"D:\Potsdam_Final\512\Images",
-    mask_path = r"D:\Potsdam_Final\512\Masks",
-    pred_path = r"D:\Potsdam_Final\Training_Results\Potsdam_512_H\predictions",
-    channels = 1
-)
+single_epoch_predictions(
+    model_path = "D:/Potsdam_Final/Training_Results/Potsdam_512_RGBI/last_weights.pt",
+    image_path = "D:/Potsdam_Final/512_test/Images",
+    mask_path = "D:/Potsdam_Final/512_test/Masks",
+    pred_path = "D:/Potsdam_Final/Training_Results/Potsdam_512_RGBI/predictions",
+    channels = 4
+    )
