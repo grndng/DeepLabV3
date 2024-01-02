bb72dd3a9b17806e66560ef6940b19521261bbf1
grndng <gueren.dinga@hcu-hamburg.de>
2024-01-02 11:06:19 +0100

    update file by paths

--- best_epoch_from_csv.py
+++ best_epoch_from_csv.py
@@ -3,7 +3,8 @@ import torch
 from model import createDeepLabv3
 
 #csv_path = r"D:\Potsdam_Final\Training_Results\Potsdam_512_H\log.csv"
-csv_path = "./Training_5ch/log.csv"
+#csv_path = "./Training_5ch/log.csv"
+csv_path = r"C:\Users\dinga\Downloads\Potsdam_1024_rgbih_weights\log.csv"
 df = pd.read_csv(csv_path)
 
 print(df.head())
